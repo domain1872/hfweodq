320b4542a93e75029295318a1804b87c0f140d4f
itzchetansingh <itzchetansingh@gmail.com>
2022-04-18 15:12:20 +0530

    sac

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ api_hash = '1defca4e6e118cbe0b8c9f61913a7fbf'
 bot_token = '5392254683:AAGDbmAUTousvvMwkEnCgwCKuFuVPmvRk_w'
 import requests
 
-url = 'https://toonmixindia.me/wp-json/wp/v2/search?per_page=5&type=post&search='
+
 
 # We have to manually call "start" if we want an explicit bot token
 bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
@@ -12,6 +12,11 @@ bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 async def my_event_handler(event):
     # if 'tmi' in event.raw_text:
     newtxt =event.raw_text
+    ids = event.chat_id
+    if ids == -1001445924604 :
+        url = 'https://toonmixindia.me/wp-json/wp/v2/search?per_page=5&type=post&search='
+    else :
+        url = 'https://coolsanime.me/wp-json/wp/v2/search?per_page=5&type=post&search='
     # y = newtxt.replace('tmi ','')
     y = newtxt
     url2 = url+y
