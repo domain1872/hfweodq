ac6bed1eb4e787291feb53b4fc13acd732daf3b5
Chetan Singh <100291522+itzchetansingh@users.noreply.github.com>
2022-04-30 09:53:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,9 @@ api_id = 16852963
 api_hash = '1defca4e6e118cbe0b8c9f61913a7fbf'
 bot_token = '5392254683:AAGDbmAUTousvvMwkEnCgwCKuFuVPmvRk_w'
 import requests
-
-
-
-# We have to manually call "start" if we want an explicit bot token
 bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 @bot.on(events.NewMessage)
 async def my_event_handler(event):
-    # if 'tmi' in event.raw_text:
     newtxt =event.raw_text
     ids = event.chat_id
     if ids == -1001445924604 :
@@ -21,9 +16,7 @@ async def my_event_handler(event):
         url = 'https://moviesrush.run/wp-json/wp/v2/search?per_page=5&type=post&search='
     else :
         url = 'https://coolsanime.me/wp-json/wp/v2/search?per_page=5&type=post&search='
-    # y = newtxt.replace('tmi ','')
-    y = newtxt
-    url2 = url+y
+    url2 = url+newtxt
     resp = requests.get(url=url2,)
     data = resp.json()
     lengthh = len(data)
