73801c964059ed80b1c84ed115292b52ac9ff5ba
Chetan Singh <100291522+itzchetansingh@users.noreply.github.com>
2022-06-17 08:55:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,13 @@
+ api_id = 16852963 # Api ID From My.Telegram.Org
+api_hash = '1defca4e6e118cbe0b8c9f61913a7fbf' # Api Hash From My.Telegram.Org
+bot_token = '5392254683:AAGDbmAUTousvvMwkEnCgwCKuFuVPmvRk_w' # Bot Token From Bot Father
+site_url = 'https://toonmixindia.me/wp-json/wp/v2/search?per_page=5&type=post&search='
 from telethon.sync import TelegramClient, events
-api_id = 16852963
-api_hash = '1defca4e6e118cbe0b8c9f61913a7fbf'
-bot_token = '5392254683:AAGDbmAUTousvvMwkEnCgwCKuFuVPmvRk_w'
 import requests
 bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 @bot.on(events.NewMessage)
 async def my_event_handler(event):
-    newtxt =event.raw_text
-    ids = event.chat_id
-    if ids == -1001445924604 :
-        url = 'https://toonmixindia.me/wp-json/wp/v2/search?per_page=5&type=post&search='
-    elif ids == -1001226029329:
-        url = 'https://moviesrush.guru/wp-json/wp/v2/search?per_page=5&type=post&search='
-    elif ids == -1001470840039:
-        url = 'https://moviesrush.guru/wp-json/wp/v2/search?per_page=5&type=post&search='
-    else :
-        url = 'https://coolsanime.me/wp-json/wp/v2/search?per_page=5&type=post&search='
-    url2 = url+newtxt
+    url2 = site_url+event.raw_text
     resp = requests.get(url=url2,)
     data = resp.json()
     lengthh = len(data)
@@ -25,6 +16,5 @@ async def my_event_handler(event):
         url1 = data[0]['url']
         replyy = f'''{title1}\n{url1}'''
         await event.reply(replyy)
-
 bot.start()
 bot.run_until_disconnected()
